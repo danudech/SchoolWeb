6404c6d95c6705832a04f37b73363e5e3b001cfc
danudech <danudech@byiteam.com>
2021-04-26 00:59:42 +0700

    Start Project - Login Page

---
+++ SchoolWeb/SchoolWeb/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SchoolWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            ViewBag.Title = "Login";
+            return View();
+        }
+    }
+}
